b29383b0d9ab590f4af4ae5b5764711465055838
Goldbox <timoteidig@yahoo.com>
2017-03-01 11:10:45 +0200

    CheckBoardKarel - Solution

--- Assignment1/CheckerboardKarel.java
+++ Assignment1/CheckerboardKarel.java
@@ -9,8 +9,82 @@
 
 import stanford.karel.*;
 
+@SuppressWarnings("serial")
 public class CheckerboardKarel extends SuperKarel {
-
-	// You fill in this part
+	
+	private boolean needsABeeper;
+	
+	public void run(){
+		putBeepersOnTheRow();
+		while(leftIsClear()){
+			moveToNextRow();
+			putBeepersOnTheRow();
+		}
+		returnToInitialRow();
+	}
+	
+	private void putBeepersOnTheRow(){
+		needsABeeper = decideTheStartingBeeper();
+		drawThisSquare();
+		while(frontIsClear()){
+			move();
+			drawThisSquare();
+		}
+		returnToBeginingOfRow();		
+	}
+	
+	private boolean decideTheStartingBeeper(){
+		if(rightIsBlocked()){
+			return true;
+		}
+		moveToPreviousRow();
+		if(beepersPresent()){
+			moveToNextRow();
+			return false;
+		} 
+		else {
+			moveToNextRow();
+			return true;
+		}
+	}
+	
+	private void moveToPreviousRow(){
+		turnRight();
+		move();
+		turnLeft();
+	}
+	
+	private void moveToNextRow(){
+		turnLeft();
+		move();
+		turnRight();
+	}
+	
+	private void drawThisSquare(){
+		if(needsABeeper){
+			putBeeper();
+			needsABeeper = false;
+		} else {
+			needsABeeper = true;
+		}
+	}
+	
+	private void returnToBeginingOfRow(){
+		turnAround();
+		moveToWall();
+		turnAround();
+	}
+	
+	private void returnToInitialRow(){
+		turnRight();
+		moveToWall();
+		turnLeft();
+	}
+	
+	private void moveToWall(){
+		while(frontIsClear()){
+			move();
+		}
+	}
 
 }
